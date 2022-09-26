f8bcf50bcb319d17e7022894c3d8d54b90e61f69
Fatih Kemal TERZİ <fatihkemalterzi@gmail.com>
2022-09-26 14:39:05 +0300

    Numerical and categorical variable analysis

--- Titanic_survivers.py
+++ Titanic_survivers.py
@@ -35,3 +35,39 @@ def grab_col_names(dataframe, cat_th=10, car_th=20):
 
 cat_cols, num_cols, cat_but_car = grab_col_names(df)
 
+
+##################################
+# KATEGORİK DEĞİŞKENLERİN ANALİZİ
+##################################
+
+#cat
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+cat_summary(df, "survived")
+
+
+##################################
+# NUMERİK DEĞİŞKENLERİN ANALİZİ
+##################################
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show()
+
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+##################################
